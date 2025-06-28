61aaad49476d3b782b249eb953e2b403bd80d250
ecks6 <andreea.oancea19@gmail.com>
2025-06-28 21:03:52 +0300

    Modern Dark Design with Turquoise Blue Theme

--- app/(main)/workspace/[id]/page.jsx
+++ app/(main)/workspace/[id]/page.jsx
@@ -18,14 +18,14 @@ const Workspace = () => {
 
             {/* Content */}
             <div className='relative z-10 p-8'>
-                <div className='grid grid-cols-1 lg:grid-cols-4 gap-8'>
-                    {/* Chat Panel - Fixed height to match code panel */}
-                    <div className='lg:col-span-1 h-[calc(100vh-4rem)]'>
+                <div className='grid grid-cols-1 lg:grid-cols-4 gap-8 h-[calc(100vh-8rem)]'>
+                    {/* Chat Panel */}
+                    <div className='lg:col-span-1'>
                         <ChatView />
                     </div>
                     
-                    {/* Code Panel - Fixed height */}
-                    <div className='lg:col-span-3 h-[calc(100vh-4rem)]'>
+                    {/* Code Panel */}
+                    <div className='lg:col-span-3'>
                         <CodeView />
                     </div>
                 </div>
